1a97cf3cbfd7b7a7820b18cd74f1c3c3fa143f2b
Anem26 <angie.ellis@utp.ac.pa>
2023-07-23 00:34:30 -0500

    Splash Screen para la app

--- app/src/main/java/com/example/semestralhuella/MainActivity.java
+++ app/src/main/java/com/example/semestralhuella/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.semestralhuella;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
+
